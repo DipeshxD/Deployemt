5adfdf7157d50cd642ad4f4aedd995eaa4548aed
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2022-02-13 21:48:07 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,5 @@
+from os import system
+
+system("git clone https://gitlab.com/loverboyXD/AstroUB && cd AstroUB && python3 -m astro")
+
+print("Heyy User... Your have Got Error in deploying AstroUB maybe you have entered Some Wrong details....!\n\nIF you want help you can Join Support: @Astro_HelpChat")
